b91d544c0ad2d6e67542552709b344a3da7cf8bc
zhaobin.zb <zhaobin.zb@alibaba-inc.com>
2023-06-29 13:02:04 +0800

    chore: 优化 welcomescreen

--- src/excalidraw-app/src/CustomWelcomeScreen.tsx
+++ src/excalidraw-app/src/CustomWelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import { WelcomeScreen, useI18n } from "@handraw/excalidraw";
-import { GithubIcon, TwitterIcon } from './icons'
+import { GithubIcon, TwitterIcon, shareWindows } from './icons'
 
 export default function CustomWelcomeScreen() {
 
@@ -12,7 +12,7 @@ export default function CustomWelcomeScreen() {
     <WelcomeScreen.Hints.ToolbarHint />
     <WelcomeScreen.Hints.HelpHint />
     <WelcomeScreen.Center>
-      <WelcomeScreen.Center.Logo />
+      <WelcomeScreen.Center.Logo>Excalidraw CN - handraw.top</WelcomeScreen.Center.Logo>
       <WelcomeScreen.Center.Heading>
         {t("welcomeScreen.app.center_heading")}
       </WelcomeScreen.Center.Heading>
@@ -23,13 +23,19 @@ export default function CustomWelcomeScreen() {
           icon={GithubIcon}
           aria-label="GitHub"
           href="https://github.com/korbinzhao/excalidraw-cn">
-          GitHub
+          {`GitHub -> give a star`} 
         </WelcomeScreen.Center.MenuItemLink>
         <WelcomeScreen.Center.MenuItemLink
           icon={TwitterIcon}
           href="https://twitter.com/korbinzhao"
           aria-label="Twitter">
-          Twitter
+          {`Twitter -> follow the author`}
+        </WelcomeScreen.Center.MenuItemLink>
+        <WelcomeScreen.Center.MenuItemLink
+          icon={shareWindows}
+          href="https://handraw.top"
+          aria-label="handraw.top">
+          {`Visit the homepage for more`}
         </WelcomeScreen.Center.MenuItemLink>
       </WelcomeScreen.Center.Menu>
     </WelcomeScreen.Center>
