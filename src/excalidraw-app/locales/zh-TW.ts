d9ecc6c80e3916dfcb7e94d7d82c514972ec8f4e
zhaobin.zb <zhaobin.zb@alibaba-inc.com>
2023-07-01 22:56:39 +0800

    chore: 文案优化

--- src/excalidraw-app/locales/zh-TW.ts
+++ src/excalidraw-app/locales/zh-TW.ts
@@ -1,6 +1,6 @@
 export default {
     welcome: {
-        github: '訪問 Githu，給個 Star 吧',
+        github: '訪問 Github，給個 Star 吧',
         twitter: '關註作者獲取最新產品動態',
         homepage: '訪問網頁版提前使用最新功能',
         coffee: '買杯咖啡/投餵貓糧'
